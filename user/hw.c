3d92258d075151d1ff6ed44c41cd63929537576a
CrayonCruncher <125257327+CrayonCruncher@users.noreply.github.com>
2023-11-10 12:07:51 +0000

    create hw.c, edit Makefile

---
+++ user/hw.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+    // Insert code here.
+    // You can use print as normal.
+    // Terminate the process.
+    printf("Hello, world!");
+    exit(0);
+}
